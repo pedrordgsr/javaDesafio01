26873bf4f0d0219cb1a41d80748c03db029ebb34
Pedro Rodrigues <pedrorodriguesssrrr@gmail.com>
2023-05-11 15:47:18 -0300

    Simplificacao

--- horas.java
+++ horas.java
@@ -1,21 +1,22 @@
-import java.util.*;
-
 public class horas {
     public static void main(String[] args) {
-        Scanner read = new Scanner(System.in);
-        System.out.println("Insira a hora");
-        int hours = read.nextInt();
-        System.out.println("Insira o minuto");
-        int minutes = read.nextInt();
-        System.out.println("Insira o segundo");
-        int seconds = read.nextInt();
 
+        //declaração de variáveis
+        int hours = 10;
+  
+        int minutes = 45;
+
+        int seconds = 30;
+
+        //equação para calcular segundos que faltam e que já passaram
         int secondsCount = seconds + minutes * 60 + hours * 3600;
         int secondsLeft = 86400 - secondsCount;
 
+        //limpar terminal
         System.out.print("\033[H\033[2J");
         System.out.flush();
 
+        //imprimir resultado
         System.out.println("Se passaram " + secondsCount + " Segundos desde o inicio do dia");
         System.out.println("Faltam " + secondsLeft + " Segundos ate o fim do dia");
     }
