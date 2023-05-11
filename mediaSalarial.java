db3c8319c8f98aed6f970fe2adb17dc7c99111e3
Pedro Rodrigues <pedrorodriguesssrrr@gmail.com>
2023-05-11 15:49:19 -0300

    Comentários na média salarial

--- mediaSalarial.java
+++ mediaSalarial.java
@@ -1,9 +1,8 @@
-
-
 public class mediaSalarial {
 
     public static void main(String[] args) {
 
+        //Declaração de variáveis
         String worker1 = "Junior";
         String worker2 = "Marcos";
         String worker3 = "José";
@@ -14,11 +13,14 @@ public class mediaSalarial {
         double worker3wage = 700.2;
         double worker4wage = 12000;
 
+        //Calculo da média
         double media = (worker1wage + worker2wage + worker3wage + worker4wage) / 4;
         
+        //Limpar Terminal
         System.out.print("\033[H\033[2J");
         System.out.flush();
         
+        //Exibir resultado no terminal
         System.out.println(worker1 + " ganha " + worker1wage + " reais");
         System.out.println(worker2 + " ganha " + worker2wage + " reais");
         System.out.println(worker3 + " ganha " + worker3wage + " reais");
